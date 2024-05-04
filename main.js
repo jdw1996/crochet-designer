1f36de5c852dfbce71b30318c79e9d04e771cbed
Joseph Daniel Winters <jdwinters96@gmail.com>
2024-05-04 16:13:55 -0400

    Slightly better Hello World

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const CANVAS = document.getElementById("canvas");
+const CTX = CANVAS.getContext("2d");
+
 const NUM_ROWS = 10;
 const NUM_COLS = 10;
+const STITCH_SIZE = 10;
+
 const PROJECT = Array.from({ length: NUM_ROWS }, (_, i) =>
   Array.from({ length: NUM_COLS }, (_, j) => ({
     row: i,
@@ -14,4 +19,16 @@ const PROJECT = Array.from({ length: NUM_ROWS }, (_, i) =>
   }))
 );
 
-console.log(PROJECT);
+for (let i = 0; i < PROJECT.length; ++i) {
+  if (PROJECT[i].length > 0 && PROJECT[i][0].isPrimaryColour) {
+    CTX.fillStyle = "black";
+  } else {
+    CTX.fillStyle = "grey";
+  }
+  CTX.fillRect(
+    0,
+    i * STITCH_SIZE,
+    PROJECT[i].length * STITCH_SIZE,
+    STITCH_SIZE
+  );
+}
