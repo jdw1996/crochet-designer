a343ef3e98d171db0e713f556e626efbf1cdf838
Joseph Daniel Winters <jdwinters96@gmail.com>
2024-05-05 11:13:44 -0400

    Styling updates

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const CTX = CANVAS.getContext("2d");
 
 const NUM_ROWS = 10;
 const NUM_COLS = 10;
-const STITCH_SIZE = 10;
+const STITCH_SIZE = 50;
 
 const PROJECT = Array.from({ length: NUM_ROWS }, (_, i) =>
   Array.from({ length: NUM_COLS }, (_, j) => ({
@@ -21,9 +21,9 @@ const PROJECT = Array.from({ length: NUM_ROWS }, (_, i) =>
 
 for (let i = 0; i < PROJECT.length; ++i) {
   if (PROJECT[i].length > 0 && PROJECT[i][0].isPrimaryColour) {
-    CTX.fillStyle = "black";
+    CTX.fillStyle = "dimgrey";
   } else {
-    CTX.fillStyle = "grey";
+    CTX.fillStyle = "whitesmoke";
   }
   CTX.fillRect(
     0,
