db19506619c6c95103c1ba8db7c89e730a9da8ed
Joseph Daniel Winters <jdwinters96@gmail.com>
2024-05-06 22:21:14 -0400

    Add toggleable dcs

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const DC_S = 2;
 const DC_SE1 = 3;
 const DC_SE2 = 4;
 
+const MILLISECONDS_PER_LOOP = 20;
 const NUM_ROWS = 10;
 const NUM_COLS = 10;
 const STITCH_SIZE = 50;
@@ -17,13 +18,38 @@ const COLOUR_SECONDARY = "whitesmoke";
 
 const PROJECT = Array.from({ length: NUM_ROWS }, (_, i) =>
   Array.from({ length: NUM_COLS }, (_, j) => ({
-    row: i,
     col: j,
+    row: i,
 
-    doubles: new Set(),
+    dcs: new Set(),
     isPrimaryColour: i % 2 === 0,
   }))
 );
+
+// LOGIC
+
+function toggleDC_S(col, row) {
+  if (
+    row < 2 ||
+    row >= NUM_ROWS ||
+    col < 0 ||
+    col >= NUM_COLS ||
+    (row !== NUM_ROWS - 1 && PROJECT[row + 1][col].dcs.has(DC_S)) ||
+    PROJECT[row - 1][col].dcs.size > 0
+  ) {
+    console.error(
+      `Error attempting to add invalid double crochet at column ${col}, row ${row}.`
+    );
+    return;
+  }
+
+  const stitch_dcs = PROJECT[row][col].dcs;
+  if (stitch_dcs.has(DC_S)) {
+    stitch_dcs.delete(DC_S);
+  } else {
+    stitch_dcs.add(DC_S);
+  }
+}
 
 // DRAWING
 
@@ -36,7 +62,7 @@ function drawStitch(stitch) {
     STITCH_SIZE
   );
 
-  if (stitch.doubles.has(DC_S)) {
+  if (stitch.dcs.has(DC_S)) {
     CTX.strokeStyle = stitch.isPrimaryColour
       ? COLOUR_PRIMARY
       : COLOUR_SECONDARY;
@@ -55,8 +81,19 @@ function drawStitch(stitch) {
   }
 }
 
-for (let i = 0; i < PROJECT.length; ++i) {
-  for (let j = 0; j < PROJECT[i].length; ++j) {
-    drawStitch(PROJECT[i][j]);
+window.setInterval(() => {
+  for (let i = 0; i < PROJECT.length; ++i) {
+    for (let j = 0; j < PROJECT[i].length; ++j) {
+      drawStitch(PROJECT[i][j]);
+    }
   }
-}
+}, MILLISECONDS_PER_LOOP);
+
+// EVENT LISTENERS
+
+CANVAS.addEventListener("mousedown", (mouseEvent) => {
+  const { offsetX, offsetY } = mouseEvent;
+  const col = Math.floor(offsetX / STITCH_SIZE);
+  const row = NUM_ROWS - Math.floor(offsetY / STITCH_SIZE);
+  toggleDC_S(col, row);
+});
