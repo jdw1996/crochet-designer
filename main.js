ec616d7d9d49f46ef88db9234578b0ba0cb4e72f
Joseph Daniel Winters <jdwinters96@gmail.com>
2024-05-06 21:51:48 -0400

    Add drawing code for dc

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 const CANVAS = document.getElementById("canvas");
 const CTX = CANVAS.getContext("2d");
 
+// Values of these don't matter; they're basically an enum.
+const DC_SW2 = 0;
+const DC_SW1 = 1;
+const DC_S = 2;
+const DC_SE1 = 3;
+const DC_SE2 = 4;
+
 const NUM_ROWS = 10;
 const NUM_COLS = 10;
 const STITCH_SIZE = 50;
@@ -13,11 +20,7 @@ const PROJECT = Array.from({ length: NUM_ROWS }, (_, i) =>
     row: i,
     col: j,
 
-    hasSW2: false,
-    hasSW1: false,
-    hasDC: false,
-    hasSE1: false,
-    hasSE2: false,
+    doubles: new Set(),
     isPrimaryColour: i % 2 === 0,
   }))
 );
@@ -25,17 +28,31 @@ const PROJECT = Array.from({ length: NUM_ROWS }, (_, i) =>
 // DRAWING
 
 function drawStitch(stitch) {
-  if (stitch.isPrimaryColour) {
-    CTX.fillStyle = COLOUR_PRIMARY;
-  } else {
-    CTX.fillStyle = COLOUR_SECONDARY;
-  }
+  CTX.fillStyle = stitch.isPrimaryColour ? COLOUR_PRIMARY : COLOUR_SECONDARY;
   CTX.fillRect(
     STITCH_SIZE * stitch.col,
-    STITCH_SIZE * stitch.row,
+    STITCH_SIZE * (NUM_ROWS - 1 - stitch.row),
     STITCH_SIZE,
     STITCH_SIZE
   );
+
+  if (stitch.doubles.has(DC_S)) {
+    CTX.strokeStyle = stitch.isPrimaryColour
+      ? COLOUR_PRIMARY
+      : COLOUR_SECONDARY;
+    CTX.lineWidth = 0.9 * STITCH_SIZE;
+
+    CTX.beginPath();
+    CTX.moveTo(
+      STITCH_SIZE * (stitch.col + 0.5),
+      STITCH_SIZE * (NUM_ROWS - stitch.row - 0.5)
+    );
+    CTX.lineTo(
+      STITCH_SIZE * (stitch.col + 0.5),
+      STITCH_SIZE * (NUM_ROWS - stitch.row + 1.5)
+    );
+    CTX.stroke();
+  }
 }
 
 for (let i = 0; i < PROJECT.length; ++i) {
